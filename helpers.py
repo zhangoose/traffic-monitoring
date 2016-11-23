fffd5f8ae7cbf47aa1e35b302aa774572b2736e8
zhangoose <hi.zhalex@Gmail.com>
2016-11-23 13:39:58 -0500

    Adding a current time window

--- helpers.py
+++ helpers.py
@@ -3,11 +3,13 @@ import curses
 
 from pytz import UTC
 
+
 def utc_now():
     """
     `now` returns a timezone aware date time in UTC .
     """
     return datetime.utcnow().replace(tzinfo=UTC)
+
 
 def format_dt(dt):
     """
@@ -24,11 +26,13 @@ def init_windows():
     current_alert = curses.newwin(20, 30, 1, 101)
     traffic = curses.newwin(20, 100, 21, 31)
     current_traffic = curses.newwin(20, 30, 21, 1)
+    current_time = curses.newwin(3, 100, 42, 1)
 
     alerts.box()
     current_alert.box()
     traffic.box()
     current_traffic.box()
+    current_time.box()
 
     alerts.addstr(1, 1, "Alert log: for average traffic", curses.A_BOLD)
     current_alert.addstr(1, 1, "Current Average Traffic for last 2 minutes", curses.A_BOLD)
@@ -39,11 +43,29 @@ def init_windows():
     current_alert.refresh()
     traffic.refresh()
     current_traffic.refresh()
+    current_time.refresh()
 
-    return alerts, current_alert, traffic, current_traffic
+    return alerts, current_alert, traffic, current_traffic, current_time
+
+
+def draw_time(current_time):
+    """
+    `draw_time` adds the current time in UTC to the `current_time` window.
+    """
+    current_time.clear()
+    current_time.box()
+
+    current_time.addstr(1, 1, format_dt(utc_now()))
+
+    current_time.refresh()
 
 
 def draw_alerts(alerts, current_alert, log_parser, alert_number):
+    """
+    `draw_alerts` adds the last 6 alert logs to the `alerts` window and adds
+    the current average traffic in the last 2 minutes to the `current_alert`
+    window.
+    """
     alerts.clear()
     current_alert.clear()
     alerts.box()
@@ -72,6 +94,10 @@ def draw_alerts(alerts, current_alert, log_parser, alert_number):
 
 
 def draw_traffic(traffic, current_traffic, log_parser):
+    """
+    `draw_traffic` adds the overall traffic to the `traffic` window and adds
+    the section name with the most traffic to the `current_traffic` window.
+    """
     traffic.clear()
     current_traffic.clear()
     traffic.box()
--- run.py
+++ run.py
@@ -6,61 +6,8 @@ import schedule
 from sh import tail
 
 from traffic import LogParser
-from helpers import init_windows, utc_now, draw_alerts, draw_traffic
+from helpers import init_windows, utc_now, draw_alerts, draw_traffic, draw_time
 
-
-"""
-def draw_alerts(alerts, current_alert, log_parser, alert_number):
-    alerts.clear()
-    current_alert.clear()
-    alerts.box()
-    current_alert.box()
-
-    message, average_traffic = log_parser.alert(alert_number)
-    messages = ""
-    for alert_log in log_parser.alert_logs[-6:]:
-        formatted_time = format_dt(alert_log[0])
-        if alert_log[1]:
-            messages += "   [{}] High traffic generated an alert - hits = {}\n".format(formatted_time, alert_log[2])
-        else:
-            messages += "   [{}] Recovered! hits = {}\n".format(formatted_time, alert_log[2])
-    
-    if average_traffic > alert_number:
-        current_alert.addstr(5, 3, str(average_traffic), curses.A_STANDOUT)
-    else:
-        current_alert.addstr(5, 3, str(average_traffic))
-
-    alerts.addstr(5, 0, messages)
-    alerts.addstr(1, 1, "Alert log: for average traffic > {}".format(alert_number), curses.A_BOLD)
-    current_alert.addstr(1, 1, "Current Average Traffic for last 2 minutes", curses.A_BOLD)
-
-    alerts.refresh()
-    current_alert.refresh()
-
-
-def draw_traffic(traffic, current_traffic, log_parser):
-    traffic.clear()
-    current_traffic.clear()
-    traffic.box()
-    current_traffic.box()
-
-    hit = log_parser.most_hits()
-    traffic_log = ""
-    if hit != (None, None):
-        current_traffic.addstr(5, 3, "{}: {}".format(hit[0], hit[1]))
-    else:
-        current_traffic.addstr(5, 3, "(none)")
-
-    for section_name, num_hits in log_parser.hits.items():
-        traffic_log += "   /{}: {}\n".format(section_name, num_hits)
-
-    traffic.addstr(5, 0, traffic_log)
-    traffic.addstr(1, 1, "Overall traffic\n(updated every 10 sec)", curses.A_BOLD)
-    current_traffic.addstr(1, 1, "Section with most traffic\n(updated every 10 sec)", curses.A_BOLD)
-    traffic.refresh()
-    current_traffic.refresh()
-
-"""
 
 parser = argparse.ArgumentParser()
 parser.add_argument("log_file", help="the access log file you want to get traffic for")
@@ -77,8 +24,9 @@ curses.noecho()
 curses.cbreak()
 screen.keypad(1)
 
-alerts, current_alert, traffic, current_traffic = init_windows()
+alerts, current_alert, traffic, current_traffic, current_time = init_windows()
 
+schedule.every(1).seconds.do(draw_time, current_time=current_time)
 schedule.every(1).seconds.do(
     draw_alerts,
     alerts=alerts,
